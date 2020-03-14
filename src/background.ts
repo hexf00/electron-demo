b5afb78371333b8ebd96a1b0f2c8645602c41a3d
hexf00 <hexf00@gmail.com>
2020-03-14 17:54:17 +0800

    build: 添加vscode调试配置项

--- src/background.ts
+++ src/background.ts
@@ -27,6 +27,8 @@ function createWindow () {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
+    
+    // 打开Chromium的开发者工具集
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
