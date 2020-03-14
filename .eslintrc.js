594fe513f96604ca9ac79a295b1979f10727826b
hexf00 <hexf00@gmail.com>
2020-03-14 20:59:55 +0800

    feat: 添加eslint和ts的配置项

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    // browser: true,
   },
   extends: [
     'plugin:vue/essential',
@@ -11,8 +12,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  globals: {
+    "__static": true,
+  },
   rules: {
+    // 生产环境时不允许有console
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 函数名后面要有空格
+    'space-before-function-paren': 'off',
+    // 是否允许多行空白
+    "no-multiple-empty-lines": "off"
   }
 }
---
+++ global.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    /**
+     * 由 vue-cli-plugin-electron-builder提供
+     * 
+     * 定义位置 ./node_modules/vue-cli-plugin-electron-builder/lib/webpackConfig.js
+     * 
+     * [在线文档](https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#handling-static-assets)
+     */
+    const __static: string
+}
+
+export {}
